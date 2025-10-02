9042eb1ba1f21d5732fb9e8e63a2d59d1b2f93db
DAHCROWN <57120458+DAHCROWN@users.noreply.github.com>
2025-10-02 15:03:42 +0100

    added types

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
+import type { Request, Response } from "express";
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
 	console.log("Received a request", req.query.name);
     const name = req.query.name;
 	res.send("Hello to you, " + name);
